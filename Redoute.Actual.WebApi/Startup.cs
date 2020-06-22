dbb0012448c7f9c0251f2fb3842bc9c6931178fb
Anibal Juniper <redoubteifly@outlook.com>
2020-06-22 21:00:54 +0800

    upd

--- Redoute.Actual.WebApi/Startup.cs
+++ Redoute.Actual.WebApi/Startup.cs
@@ -31,6 +31,28 @@ namespace Redoute.Actual.WebApi
             services.AddSingleton(typeof(Actualsis.IRepositonry.IBasicRepository<>), typeof(Actualsis.Repositonry.BasicRepository<>));
             services.AddSingleton<ISysUserService, SysUserService>();
 
+            #region 依赖注入
+
+            //var builder = new ContainerBuilder();//实例化容器
+            ////注册所有模块module
+            //builder.RegisterAssemblyModules(Assembly.GetExecutingAssembly());
+            ////获取所有的程序集
+            ////var assemblys = BuildManager.GetReferencedAssemblies().Cast<Assembly>().ToArray();
+            //var assemblys = RuntimeHelper.GetAllAssemblies().ToArray();
+
+            ////注册所有继承IDependency接口的类
+            //builder.RegisterAssemblyTypes().Where(type => typeof(IDependency).IsAssignableFrom(type) && !type.IsAbstract);
+            ////注册仓储，所有IRepository接口到Repository的映射
+            //builder.RegisterAssemblyTypes(assemblys).Where(t => t.Name.EndsWith("Repository") && !t.Name.StartsWith("I")).AsImplementedInterfaces();
+            ////注册服务，所有IApplicationService到ApplicationService的映射
+            ////builder.RegisterAssemblyTypes(assemblys).Where(t => t.Name.EndsWith("AppService") && !t.Name.StartsWith("I")).AsImplementedInterfaces();
+            //builder.Populate(services);
+            //ApplicationContainer = builder.Build();
+
+            //return new AutofacServiceProvider(ApplicationContainer); //第三方IOC接管 core内置DI容器 
+            ////return services.BuilderInterceptableServiceProvider(builder => builder.SetDynamicProxyFactory());
+            #endregion
+
             //返回json  大小写
             services.AddMvc().AddJsonOptions(op => op.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver());
 
